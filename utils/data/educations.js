58a465949385e4724cb0802683536d4a2bb5a034
JasperMunene <jaspermunene2022@gmail.com>
2024-06-06 21:36:46 +0300

    change

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,12 +1,7 @@
 export const educations = [
+ 
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "Currently...",
-    institution: "Multimedia University of Kenya",
-  },
-  {
-    id: 2,
     title: "Web Development Bootcamp",
     duration: "",
     institution: "Udemy",
