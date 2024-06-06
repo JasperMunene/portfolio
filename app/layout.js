65780a45a44b2bd1669539f49617286a8f867c2c
JasperMunene <jaspermunene2022@gmail.com>
2024-06-06 15:51:44 +0300

    changes

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
       </head>
-      <body className='font-lilita'>
+      <body className='font-mono'>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
