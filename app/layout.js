1eda4177f8896a13f189a9e0335cafcec0b18c8e
JasperMunene <jaspermunene2022@gmail.com>
2024-06-06 10:44:04 +0300

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 import { GoogleTagManager } from '@next/third-parties/google';
-import { Inter,Lilita_One, } from 'next/font/google';
+import { Lilita_One } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import './css/card.scss';
 import './css/globals.scss';
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+
 const lilitaOne = Lilita_One({
   subsets: ['latin'],
   weight: ['400'],
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${lilitaOne.variable}`}>
+      <body className={`${lilitaOne.variable}`}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
