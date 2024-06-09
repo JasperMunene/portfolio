180a92ceda70ff27acf2ebdacf91c9f8bd051d54
JasperMunene <jaspermunene2022@gmail.com>
2024-06-09 13:21:46 +0300

    changes

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Jasper Munene",
   profile: '/profiles.png',
   designation: "Software Developer",
-  description: `Hey there! I'm Jasper Munene, your friendly neighborhood code ninja and software whisperer. From writing my first "Hello, World!" to mastering the art of Python and JavaScript, I've been on an epic quest to turn caffeine into code and ideas into reality. I love tackling tech challenges and crafting solutions that make life easier—and a bit more awesome!When I'm not glued to my screen debugging or building something cool, you can catch me vibing to some rap music (Kendrick Lamar is my jam! 😉😁), sketching some digital art, or leveling up in the latest video games. Life's an adventure, and I'm all about mixing creativity with tech wizardry. Let's team up and create some digital magic together! 🚀`,
+  description: `Hey there! I'm Jasper Munene, your friendly neighborhood code ninja and software whisperer. From writing my first "Hello, World!" to mastering the art of Python and JavaScript, I've been on an epic quest to turn caffeine into code and ideas into reality. I love tackling tech challenges and crafting solutions that make life easier—and a bit more awesome!When I'm not glued to my screen debugging or building something cool, you can catch me vibing to some rap music, sketching some digital art, or leveling up in the latest video games. Life's an adventure, and I'm all about mixing creativity with tech wizardry. Let's team up and create some digital magic together! 🚀`,
   email: 'devjaspermunene@gmail.com',
   phone: '+254706071639',
   address: 'Nairobi, Kenya ',
