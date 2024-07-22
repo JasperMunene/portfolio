bc25812ece09fa503d75e881dd7d7626340be720
JasperMunene <jaspermunene2022@gmail.com>
2024-07-22 09:48:41 +0300

    changes

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,6 @@
 export const experiences = [
-  {
+ {
     id: 1,
-    title: "IT technician",
-    company: "PCEA Nanyuki",
-    duration: "( Jan 2023 - Dec 2023)"
-  },
-  {
-    id: 2,
     title: "Fullstack Developer",
     company: "Freelance.",
     duration: "(2024)"
